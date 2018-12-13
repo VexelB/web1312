c80b3deff3e46c72bbc6bf4c764a5b046f4ae70b
vexelb <vexelb@gmail.com>
2018-12-13 14:26:08 +0300

    dobavil vse

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { get } = require('axios');
+
+const PORT = 4321;
+const app = express();
+app
+  .get(/hello/, r => r.res.end('Hello world!'))
+  .get(/users/, async (r) => {
+    const { data: { users: items } } = await get(URL);
+    r.res.render('list', { title: 'Список логинов', items });
+  })
+  .use(r => r.res.status(404).end('Soryan ya ne tut'))
+  .use((e, r, res) => res.status(500).end(`Error: ${e}`))
+  .set('view engine', 'pug')
+  .listen(process.env.PORT || PORT, () => console.log(process.pid));
